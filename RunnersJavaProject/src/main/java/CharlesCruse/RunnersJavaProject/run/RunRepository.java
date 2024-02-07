dae1a59896bb98f5e992911b4b801c09bda44b7a
Charles Cruse <charle9@vt.edu>
2024-02-07 17:08:36 -0500

    Adjusted returned run to be an Optional

--- RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunRepository.java
+++ RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
@@ -16,11 +17,10 @@ public class RunRepository {
         return runs;
     }
 
-    Run findById(Integer id) {
+    Optional<Run> findById(Integer id) {
         return runs.stream()
                 .filter(run -> run.id().equals(id))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
     @PostConstruct
