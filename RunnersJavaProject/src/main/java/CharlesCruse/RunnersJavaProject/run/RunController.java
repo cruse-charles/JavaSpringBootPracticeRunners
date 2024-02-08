9a47f9d15df8f89d37203c27a4cd85a85b97fef8
Charles Cruse <charle9@vt.edu>
2024-02-08 16:58:57 -0500

    Added endpoint to find specific run

--- RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunController.java
+++ RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunController.java
@@ -1,11 +1,14 @@
 package CharlesCruse.RunnersJavaProject.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/runs")
 public class RunController {
 
     private final RunRepository runRepository;
@@ -14,9 +17,14 @@ public class RunController {
         this.runRepository = runRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping("")
     List<Run> findAll() {
         return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run findOne(@PathVariable Integer id) {
+        return runRepository.findById(id);
     }
 
 }
