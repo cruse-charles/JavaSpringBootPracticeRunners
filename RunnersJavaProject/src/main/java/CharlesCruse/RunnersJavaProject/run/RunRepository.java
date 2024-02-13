a294a80fc283ed33c17524c47d6d9ced2e533d59
Charles Cruse <charle9@vt.edu>
2024-02-12 22:13:40 -0500

    Created runs and findAlll method

--- RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunRepository.java
+++ RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunRepository.java
@@ -1,11 +1,24 @@
 package CharlesCruse.RunnersJavaProject.run;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
 
+    List<Run> findAll() {
+        return runs;
+    }
 
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1, "Morning Run", Location.OUTDOOR, LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5));
+        runs.add(new Run(2, "Evening Run", Location.INDOOR, LocalDateTime.now(), LocalDateTime.now().plusHours(1), 3));
+    }
 }
