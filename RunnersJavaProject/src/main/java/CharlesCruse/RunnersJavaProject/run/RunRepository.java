7170e64a425608d1a2368c55af26173674664b1f
Charles Cruse <charle9@vt.edu>
2024-02-13 22:10:06 -0500

    Creating RunRepository

---
+++ RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/run/RunRepository.java
@@ -0,0 +1,11 @@
+package CharlesCruse.RunnersJavaProject.run;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+
+}
