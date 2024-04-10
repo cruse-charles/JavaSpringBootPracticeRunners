ac1843f9e7f1d07de8017d2f696b2a8248938f6e
Charles Cruse <charle9@vt.edu>
2024-04-09 22:50:29 -0400

    Added Run class

--- RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/Application.java
+++ RunnersJavaProject/src/main/java/CharlesCruse/RunnersJavaProject/Application.java
@@ -1,13 +1,30 @@
 package CharlesCruse.RunnersJavaProject;
 
+import CharlesCruse.RunnersJavaProject.run.Location;
+import CharlesCruse.RunnersJavaProject.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "Morning Run", Location.OUTDOOR, LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5);
+			log.info("Run: " + run);
+		};
+	}
 }
